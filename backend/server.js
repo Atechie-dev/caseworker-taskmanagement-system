b8016c5a82ace55f3863ce6b76a934707c6510c9
Atechie-dev <annontechie@gmail.com>
2026-05-23 15:06:15 +0100

    Added the db.js with the code in config folder,updated the server.js file and the .env file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
-const app = require("./app");
+require("dotenv").config();
 
-const PORT = 5050;
+const app = require("./app");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+//DB URL:mongodb+srv://annontechie_db_user:0Iq2u7xUzHJ7e23b@cluster0.7rbqirb.mongodb.net/?appName=Cluster0
